f85af84ad423f4a93465b49c821f88baeafe5e5a
Battlehart <sarah.stelljes@gmail.com>
2022-09-04 15:15:53 -0400

    renamed routes folder to controllers. Began working on front end via handlebars. Worked on homepage.handlebars and main.handlebars.

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequalize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+// set up engines
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequalize.sync({ force: false }).then(() => {
